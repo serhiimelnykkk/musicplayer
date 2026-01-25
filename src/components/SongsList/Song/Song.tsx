ecde97ef14a75f5f6163a0b7a4d114cf24fffa9a
serhiimelnyk <smrlyman@gmail.com>
2026-01-25 15:19:41 +0200

    feat: add play button, update styles

--- src/components/SongsList/Song/Song.tsx
+++ src/components/SongsList/Song/Song.tsx
@@ -1,4 +1,5 @@
 import { type Song as SongType } from "@/types";
+import { Play } from "lucide-react";
 
 interface Props {
   song: SongType;
@@ -10,15 +11,20 @@ export const Song = ({ song }: Props) => {
   const duration = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 
   return (
-    <li className="flex font-semibold gap-4 p-4 bg-neutral-900 text-neutral-100 border-b border-neutral-700">
-      <img
-        src={song.albumCover}
-        alt={`${song.title}'s album cover`}
-        width={64}
-        height={64}
-        className="size-16 object-cover rounded-sm"
-        loading="lazy"
-      />
+    <li className="flex group font-semibold gap-4 p-4 bg-neutral-900 text-neutral-100 border-b border-neutral-700">
+      <div className="size-16">
+        <button className="relative">
+          <img
+            src={song.albumCover}
+            alt={`${song.title}'s album cover`}
+            width={64}
+            height={64}
+            className="object-cover rounded-sm"
+            loading="lazy"
+          />
+          <Play className="absolute top-[50%] left-[50%] -translate-[50%] opacity-0 group-hover:opacity-100 transition-opacity duration-200" />
+        </button>
+      </div>
       <div className="grid grid-cols-4 gap-4 flex-1 items-center">
         <div className="flex flex-col self-stretch">
           <span className="text-lg flex-1 font-bold overflow-hidden whitespace-nowrap text-ellipsis">
