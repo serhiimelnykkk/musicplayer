b373837e60081d96bb8d86d63599acf466f4c6d0
serhiimelnyk <smrlyman@gmail.com>
2026-01-26 18:23:49 +0200

    perf: optimize Song by lifting up context and setting up memo comparator

--- src/components/SongsList/Song/Song.tsx
+++ src/components/SongsList/Song/Song.tsx
@@ -1,6 +1,4 @@
-import { useCurrentSong } from "@/context/CurrentSongContext/CurrentSongContext";
 import { useHowl } from "@/context/HowlRefContext/HowlRefContext";
-import { useIsPlaying } from "@/context/IsPlayingContext/IsPlayingContext";
 import { type Song as SongType } from "@/types";
 import { Pause, Play } from "lucide-react";
 import { memo } from "react";
@@ -8,6 +6,9 @@ import { memo } from "react";
 interface Props {
   song: SongType;
   isActive: boolean;
+  isPlaying: boolean;
+  setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>;
+  setCurrentSongId: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
 const durationToViewString = (initialDuration: number) => {
@@ -18,61 +19,68 @@ const durationToViewString = (initialDuration: number) => {
   return duration;
 };
 
-export const Song = memo(({ song, isActive }: Props) => {
-  const howlRef = useHowl();
-  const { setCurrentSongId } = useCurrentSong();
-  const duration = durationToViewString(song.duration);
-  const { isPlaying, setIsPlaying } = useIsPlaying();
+export const Song = memo(
+  ({ song, isActive, isPlaying, setIsPlaying, setCurrentSongId }: Props) => {
+    const howlRef = useHowl();
+    const duration = durationToViewString(song.duration);
 
-  const handlePlayClick = () => {
-    if (howlRef.current && isActive) {
-      const playing = howlRef.current.playing();
-      if (playing) {
-        howlRef.current.pause();
-      } else {
-        howlRef.current.play();
+    const handlePlayClick = () => {
+      if (howlRef.current && isActive) {
+        const playing = howlRef.current.playing();
+        if (playing) {
+          howlRef.current.pause();
+        } else {
+          howlRef.current.play();
+        }
+        setIsPlaying(!playing);
       }
-      setIsPlaying(!playing);
-    }
-    if (!isActive) {
-      setIsPlaying(true);
-    }
-    setCurrentSongId(song.id);
-  };
+      if (!isActive) {
+        setIsPlaying(true);
+      }
+      setCurrentSongId(song.id);
+    };
 
-  return (
-    <li
-      className={`flex group font-semibold gap-4 p-4 bg-neutral-900 text-neutral-100 border-b border-neutral-700`}
-    >
-      <div className="size-16">
-        <button className="relative" onClick={handlePlayClick}>
-          <img
-            src={song.albumCover}
-            alt={`${song.title}'s album cover`}
-            width={64}
-            height={64}
-            className="object-cover rounded-sm"
-            loading="lazy"
-          />
-          <div className="absolute top-[50%] left-[50%] -translate-[50%] opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-            {isPlaying && isActive ? <Pause /> : <Play />}
-          </div>
-        </button>
-      </div>
-      <div className="grid grid-cols-4 gap-4 flex-1 items-center">
-        <div className="flex flex-col self-stretch">
-          <span
-            className={`${isActive ? "text-green-500" : "group-hover:text-teal-500"} transition-colors duration-200 text-lg flex-1 font-bold overflow-hidden whitespace-nowrap text-ellipsis`}
-          >
-            {song.title}
-          </span>
-          <span className="text-sm text-neutral-500">{song.artist}</span>
+    return (
+      <li
+        className={`flex group font-semibold gap-4 p-4 bg-neutral-900 text-neutral-100 border-b border-neutral-700`}
+      >
+        <div className="size-16">
+          <button className="relative" onClick={handlePlayClick}>
+            <img
+              src={song.albumCover}
+              alt={`${song.title}'s album cover`}
+              width={64}
+              height={64}
+              className="object-cover rounded-sm"
+              loading="lazy"
+            />
+            <div className="absolute top-[50%] left-[50%] -translate-[50%] opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+              {isPlaying && isActive ? <Pause /> : <Play />}
+            </div>
+          </button>
         </div>
+        <div className="grid grid-cols-4 gap-4 flex-1 items-center">
+          <div className="flex flex-col self-stretch">
+            <span
+              className={`${isActive ? "text-green-500" : "group-hover:text-teal-500"} transition-colors duration-200 text-lg flex-1 font-bold overflow-hidden whitespace-nowrap text-ellipsis`}
+            >
+              {song.title}
+            </span>
+            <span className="text-sm text-neutral-500">{song.artist}</span>
+          </div>
 
-        <span>{song.albumName}</span>
-        <span>{song.genre}</span>
-        <span className="justify-self-end">{duration}</span>
-      </div>
-    </li>
-  );
-});
+          <span>{song.albumName}</span>
+          <span>{song.genre}</span>
+          <span className="justify-self-end">{duration}</span>
+        </div>
+      </li>
+    );
+  },
+  (prevProps, newProps) => {
+    if (prevProps.isActive === false && newProps.isActive === false) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+);
--- src/components/SongsList/SongsList.tsx
+++ src/components/SongsList/SongsList.tsx
@@ -1,15 +1,24 @@
 import { Song } from "@/components/SongsList/Song/Song";
-import { useSongs } from "@/context/SongsContext/SongsContext";
 import { useCurrentSong } from "@/context/CurrentSongContext/CurrentSongContext";
+import { useIsPlaying } from "@/context/IsPlayingContext/IsPlayingContext";
+import { useSongs } from "@/context/SongsContext/SongsContext";
 
 export const SongsList = () => {
   const { songs } = useSongs();
-  const { currentSongId } = useCurrentSong();
+  const { currentSongId, setCurrentSongId } = useCurrentSong();
+  const { isPlaying, setIsPlaying } = useIsPlaying();
 
   return (
     <ul className="list-none">
       {songs.map((song) => (
-        <Song key={song.id} song={song} isActive={song.id === currentSongId} />
+        <Song
+          key={song.id}
+          song={song}
+          isActive={song.id === currentSongId}
+          isPlaying={isPlaying}
+          setIsPlaying={setIsPlaying}
+          setCurrentSongId={setCurrentSongId}
+        />
       ))}
     </ul>
   );
