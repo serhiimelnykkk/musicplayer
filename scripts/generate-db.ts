da3c2c72d95fb272d19f6f124ff2fc68840983ee
serhiimelnyk <smrlyman@gmail.com>
2026-02-16 21:26:12 +0200

    fix: use relative paths

--- scripts/generate-db.ts
+++ scripts/generate-db.ts
@@ -4,38 +4,43 @@ import { parseFile } from "music-metadata";
 import path from "path";
 import { type Song } from "../src/types";
 
-const createSong = async (filePath: string): Promise<Song> => {
-  const metadata = await parseFile(filePath);
+// scripts/generate-db.ts
+
+const createSong = async (
+  songsFolderPath: string,
+  fileName: string,
+): Promise<Song> => {
+  const fullFilePath = path.join(songsFolderPath, fileName);
+  const metadata = await parseFile(fullFilePath);
 
   const duration = metadata.format.duration || 0;
 
-  const song: Song = {
+  return {
     id: faker.string.uuid(),
     albumCover: faker.image.urlPicsumPhotos({ width: 64, height: 64 }),
     albumName: faker.music.album(),
     artist: faker.music.artist(),
-    filePath: filePath,
+    filePath: `/music/${fileName}`,
     genre: faker.music.genre(),
     title: faker.music.songName(),
     duration: duration,
   };
-
-  return song;
 };
 
 const generateDb = async () => {
   const songsFolderPath = path.join(process.cwd(), "public", "music");
-  const songsPaths = fs.readdirSync(songsFolderPath);
+  const songsPaths = fs
+    .readdirSync(songsFolderPath)
+    .filter((file) => !file.startsWith("."));
 
-  const promises = songsPaths.map((path) =>
-    createSong(`${songsFolderPath}/${path}`),
+  const promises = songsPaths.map((fileName) =>
+    createSong(songsFolderPath, fileName),
   );
 
   const songs = await Promise.all(promises);
-
   const db = { songs };
-  const outputPath = path.join(process.cwd(), "public", "db.json");
 
+  const outputPath = path.join(process.cwd(), "public", "db.json");
   fs.writeFileSync(outputPath, JSON.stringify(db, null, 2));
 };
 
