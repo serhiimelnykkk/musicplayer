40be1abe0cd7fed618a4a1d2c09014de9f9fdead
serhiimelnyk <smrlyman@gmail.com>
2026-02-18 21:37:56 +0200

    feat: add playlist actions

--- src/store/playlistStore.ts
+++ src/store/playlistStore.ts
@@ -2,7 +2,7 @@ import type { Song } from "@/types";
 import { create } from "zustand";
 
 interface Playlist {
-id: string;
+  id: string;
   name: string;
   songIds: Set<Song["id"]>;
 }
@@ -13,11 +13,41 @@ interface State {
 
 interface Actions {
   setState(update: Partial<State>): void;
+  addSong: (playlistId: Playlist["id"], songId: Song["id"]) => void;
+  createPlaylist: (name?: Playlist["name"]) => void;
 }
 
 const initialState: State = { playlists: [] };
 
 export const usePlaylists = create<State & Actions>((set) => ({
   ...initialState,
+  createPlaylist: (name?: Playlist["name"]) =>
+    set((state) => {
+      const newPlaylist: Playlist = {
+        id: crypto.randomUUID(),
+        name: name || "Unnamed Playlist",
+        songIds: new Set(),
+      };
+      return { ...state, playlists: [...state.playlists, newPlaylist] };
+    }),
+  addSong: (playlistId: Playlist["id"], songId: Song["id"]) =>
+    set((state) => {
+      const playlist = state.playlists.find(
+        (playlist) => playlist.id === playlistId,
+      );
+
+      if (!playlist) {
+        return state;
+      }
+
+      playlist.songIds.add(songId);
+      const playlists = state.playlists;
+      const updatedPlaylists = playlists.filter(
+        (playlist) => playlist.id === playlistId,
+      );
+      updatedPlaylists.push(playlist);
+
+      return { ...state, playlists: updatedPlaylists };
+    }),
   setState: (update) => set((state) => ({ ...state, ...update })),
 }));
