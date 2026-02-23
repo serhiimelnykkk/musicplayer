d402c706d8a624c3ebe904d65762b75b82b8f97e
serhiimelnyk <smrlyman@gmail.com>
2026-02-23 15:24:28 +0200

    feat: update styles

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -11,7 +11,7 @@ export const Main = () => {
       <div className="overflow-y-auto h-full p-4 border-r border-neutral-600 bg-neutral-900 text-neutral-100">
         <PlaylistsList />
       </div>
-      <div className="overflow-y-scroll h-full">
+      <div className="overflow-y-scroll h-full bg-neutral-900">
         <DialogRoot />
         <div className="sticky bottom-0 left-0 right-0">
           <Player />
