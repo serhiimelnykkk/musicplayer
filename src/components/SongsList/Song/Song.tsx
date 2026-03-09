b2f3376916089c04563f49ff01d0c7f7108e1966
serhiimelnyk <smrlyman@gmail.com>
2026-03-09 08:26:13 +0200

    feat: collapse text when it doesn't fit in container

--- src/components/SongsList/Song/Song.tsx
+++ src/components/SongsList/Song/Song.tsx
@@ -25,18 +25,24 @@ export const Song = memo(({ song }: Props) => {
       className={`flex group font-semibold gap-4 p-4 bg-neutral-900 text-neutral-100 border-b border-neutral-700 items-center`}
     >
       <PlayButton song={song} />
-      <div className="grid grid-cols-4 gap-4 flex-1 items-center text-sm">
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 flex-1 items-center text-sm">
         <div className="flex flex-col self-stretch">
           <span
             className={`${isActive ? "text-green-500" : "group-hover:text-teal-500"} text-base transition-colors duration-200 flex-1 font-bold overflow-hidden whitespace-nowrap text-ellipsis`}
           >
             {song.title}
           </span>
-          <span className="text-sm text-neutral-500">{song.artist}</span>
+          <span className="text-sm text-neutral-500 overflow-hidden whitespace-nowrap text-ellipsis">
+            {song.artist}
+          </span>
         </div>
-        <span>{song.albumName}</span>
-        <span>{song.genre}</span>
-        <div className="justify-self-end flex items-center gap-4">
+        <span className="overflow-hidden whitespace-nowrap text-ellipsis hidden lg:block">
+          {song.albumName}
+        </span>
+        <span className="overflow-hidden whitespace-nowrap text-ellipsis hidden lg:block">
+          {song.genre}
+        </span>
+        <div className="justify-self-end flex items-center gap-4 overflow-hidden whitespace-nowrap text-ellipsis">
           <span>{duration}</span>
           <button
             onClick={() => handleAddToPlaylistClick()}
